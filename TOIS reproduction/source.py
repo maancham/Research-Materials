c2e56da6db3fcf73d2f20036603f5b3ffae2969f
Houmaan Chamani <houmaanch@gmail.com>
2023-03-27 14:05:48 -0300

    TOIS reporting functionality added

--- TOIS reproduction/source.py
+++ TOIS reproduction/source.py
@@ -1,5 +1,6 @@
 from recbole.quick_start import run_recbole
 import sys
+import pandas as pd
 from pathlib import Path
 import yaml
 
@@ -9,6 +10,8 @@ import yaml
 def ModelHandler(models):
 
     dataset_config =  'ml-1m.yaml'
+    model_dict = {}
+
     for model in models:
 
         if (model in ['Pop', 'ItemKNN']):
@@ -29,11 +32,21 @@ def ModelHandler(models):
                 config_file = yaml.full_load(config_file)
 
         config_dict = {**config_dict, **config_file}
-        run_recbole(dataset='ml-1m', model=model, config_dict = config_dict, saved=False)
+        result = run_recbole(dataset='ml-1m', model=model, config_dict = config_dict, saved=False)
+
+        model_result_list = []
+        if (result['best_valid_result']):
+            model_result_list.append(result['best_valid_result']['ndcg@10'])
+        else:
+            model_result_list.append('None')
+        model_result_list.append(result['test_result']['ndcg@10'])
 
 
-
-
+        model_dict[model] = model_result_list
+        
+    df = pd.DataFrame.from_dict(model_dict, orient='index')
+    df.columns = ['valid_ndcg', 'test_ndcg']
+    return df
 
 
 if __name__ == '__main__':
@@ -41,4 +54,5 @@ if __name__ == '__main__':
     models = ['Pop', 'ItemKNN', 'BPR', 'NeuMF', 'CDAE', 'ENMF',
               'FISM', 'LightGCN', 'MultiVAE', 'NAIS', 'NGCF']
     
-    ModelHandler(models)
+    result_df = ModelHandler(models)
+    print(result_df)
